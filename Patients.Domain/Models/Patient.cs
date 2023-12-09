675823852b157016a091697830e81562d43faa88
brianackley001 <brian.ch.ackley@gmail.com>
2023-12-09 09:45:53 -0800

    Add project files.

---
+++ Patients.Domain/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? GenderDescription { get; set; } = "Rather Not Say";
+        public DateTime? CreatedDate { get; set; } = default;
+        public DateTime? UpdatedDate { get; set; } = default;
+        public DateTime BirthDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
