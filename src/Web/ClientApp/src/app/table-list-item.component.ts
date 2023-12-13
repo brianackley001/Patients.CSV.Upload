ddff3a5f57c4abfe528051b96197f9698b7af156
brianackley001 <brian.ch.ackley@gmail.com>
2023-12-12 22:54:08 -0800

    Add form fields + ngModel  to EDIT modal

--- src/Web/ClientApp/src/app/table-list-item.component.ts
+++ src/Web/ClientApp/src/app/table-list-item.component.ts
@@ -1,5 +1,6 @@
- import { Component, Input } from '@angular/core';
- import { NgbHighlight, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+ import { Component, Input, inject, TemplateRef  } from '@angular/core';
+ import { NgbHighlight, ModalDismissReasons, NgbModal  } from '@ng-bootstrap/ng-bootstrap';
+ import { FormsModule } from '@angular/forms'; // Import FormsModule
  
 import { MmDdYYYYDatePipe } from './pipes/mm--dd-yyyy-date.pipe';
 import { Patient } from './dataModels/patient';
@@ -7,11 +8,45 @@ import { Patient } from './dataModels/patient';
 @Component({
   selector: 'tr[table-list-item]',
   standalone: true,
-  imports: [ MmDdYYYYDatePipe,NgbHighlight],
+  imports: [ MmDdYYYYDatePipe, NgbHighlight, FormsModule],
   templateUrl: './table-list-item.component.html',
   styleUrl: './table-list-item.component.css'
 })
 export class TableListItem{
   @Input() patient!: Patient;
   @Input() ngbSearchTerm!: string;
+	private modalService = inject(NgbModal);
+	closeResult = '';
+	open(content: TemplateRef<any>) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+  openVerticallyCentered(content: TemplateRef<any>) {
+		this.modalService.open(content, { centered: true }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+	private getDismissReason(reason: any): string {
+		switch (reason) {
+			case ModalDismissReasons.ESC:
+				return 'by pressing ESC';
+			case ModalDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on a backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+	}
 }
